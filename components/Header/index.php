16ad4f3bd5f8265ebf0b59e48650f0a1f2f818c4
Willian Simas <163364952+simasguga@users.noreply.github.com>
2024-06-25 09:17:37 -0300

    arquivo home e componente header

---
+++ components/Header/index.php
@@ -0,0 +1,11 @@
+<header class="headerTag">
+    <nav class="navTag">
+        <li class="liNavTag">Logo</li>
+        <li class="liNavTag">Sobre</li>
+        <li class="liNavTag">Contato</li>
+        <li class="liNavTag">Solicitar Doação</li>
+        <li class="liNavTag">Realizar Doação</li>
+        <li class="liNavTag">Localidades</li>
+        <li class="liNavTag">Trabalhos Realizados</li>
+    </nav>
+</header>
